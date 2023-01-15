60be9f67465375aeff7f5250f43e2a2393ec9cbe
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2023-01-15 23:34:30 +1100

    changed text and card UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { downloadVideo } from "./helper/axioshelper";
 import { Button } from "@nextui-org/react";
 import { Input, Grid } from "@nextui-org/react";
 import { Text } from "@nextui-org/react";
+import { Card } from "@nextui-org/react";
 function App() {
   const [form, setForm] = useState({});
   const handleOnSubmit = async (e) => {
@@ -20,58 +21,42 @@ function App() {
   };
   return (
     <div className="App">
-      <>
-        <Text
-          h1
-          size={60}
-          css={{
-            textGradient: "45deg, $blue600 -20%, $pink600 50%",
-          }}
-          weight="bold"
-        >
-          Download
-        </Text>
-        <Text
-          h1
-          size={60}
-          css={{
-            textGradient: "45deg, $purple600 -20%, $pink600 100%",
-          }}
-          weight="bold"
-        >
-          Youtube Videos
-        </Text>
-        <Text
-          h1
-          size={60}
-          css={{
-            textGradient: "45deg, $yellow600 -20%, $red600 100%",
-          }}
-          weight="bold"
-        >
-          in MP3
-        </Text>
-      </>
-      {/* Create a Form Input for the url of youtube*/}
-      <form className="form spc" onSubmit={handleOnSubmit}>
-        {/* <input
+      {/* Wrap inside card */}
+      <Card css={{ mw: "600px" }}>
+        <Card.Body>
+          <>
+            <Text h1 size={60} weight="bold">
+              Download
+            </Text>
+            <Text h1 size={60} weight="bold">
+              Youtube Videos
+            </Text>
+            <Text h1 size={60} weight="bold">
+              in MP3
+            </Text>
+          </>
+          {/* Create a Form Input for the url of youtube*/}
+          <form className="form spc" onSubmit={handleOnSubmit}>
+            {/* <input
           type="text"
           name="url"
           placeholder="Enter Youtube URL"
           onChange={handleOnChange}
         /> */}
-        <Input
-          bordered
-          placeholder="Youtube URL"
-          type="text"
-          name="url"
-          onChange={handleOnChange}
-          className="spc1"
-        />
-        <Button color="success" type="submit" className="siz">
-          Submit
-        </Button>
-      </form>
+            <Input
+              bordered
+              placeholder="Youtube URL"
+              type="text"
+              name="url"
+              onChange={handleOnChange}
+              className="spc1"
+            />
+            <Button color="success" type="submit" className="siz">
+              Submit
+            </Button>
+          </form>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
