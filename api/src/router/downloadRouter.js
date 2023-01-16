f3bdf65d79692395cf34e99327b9a88306a6e139
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2023-01-16 23:02:01 +1100

    directed the download to desktop

--- api/src/router/downloadRouter.js
+++ api/src/router/downloadRouter.js
@@ -1,12 +1,14 @@
 import express, { Router } from "express";
 import ytdl from "ytdl-core";
 const route = express.Router();
+import os from "os";
 import fs from "fs";
 route.post("/", async (req, res, next) => {
   // get request by sending url for youtube video
   try {
     const r = req.body.url;
-
+    const homeDir = os.homedir(); // See: https://www.npmjs.com/package/os
+    const desktopDir = `${homeDir}/Desktop`;
     console.log(r);
     if (r == null) {
       return res.status(400).json({
@@ -15,7 +17,7 @@ route.post("/", async (req, res, next) => {
       });
     }
     ytdl(r, { filter: "audioonly" })
-      .pipe(fs.createWriteStream("video.mp3"))
+      .pipe(fs.createWriteStream(`${desktopDir}\\video.mp3`))
       .on("error", function (err) {
         console.log(err);
       });
